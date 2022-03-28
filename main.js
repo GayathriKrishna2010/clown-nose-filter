3918c942178cbd03b1ee363712d379076a62a109
GayathriKrishna2010 <91413553+GayathriKrishna2010@users.noreply.github.com>
2022-03-28 14:40:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+noseX = 0;
+noseY = 0;
+
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(450,450);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300 , 300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses)
+}
+
+function modelLoaded() {
+    console.log("model Loaded");
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log(results);
+        console.log("nose X = " + noseY);
+        console.log("nose Y =" + noseY);
+    }
+}
+
+function draw() {
+    image(video , 0 , 0 , 300 , 300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(noseX , noseY , 300 , 300);
+}
+
+function take_snapshot() {
+    save('Your_Filter_img.png')
+}
